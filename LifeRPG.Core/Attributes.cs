e829f25d8b419b885d6dc66299b62e7f1d148884
MR.White__td <baiqiao@tutamail.com>
2026-08-24 21:06:47 +0800

    Add LifeRPG attributes model

---
+++ LifeRPG.Core/Attributes.cs
@@ -0,0 +1,14 @@
+namespace LifeRPG.Core;
+
+/// <summary>
+/// Long-term growth dimensions.
+/// Values will later be calculated from TimeTracker activities.
+/// </summary>
+public class Attributes
+{
+    public int Intelligence { get; set; }
+    public int Discipline { get; set; }
+    public int Fitness { get; set; }
+    public int Creativity { get; set; }
+    public int Social { get; set; }
+}
